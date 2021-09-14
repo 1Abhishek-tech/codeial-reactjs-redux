b44ba7350e75b2676f76072d777fc074d20d67b5
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2021-09-14 08:55:41 +0530

    Adding Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,58 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <PostsList posts={this.props.posts} />
       </div>
     );
@@ -22,7 +74,7 @@ function mapStateToProp(state) {
   };
 }
 
-App.protoTypes = {
+App.propTypes = {
   posts: PropTypes.array.isRequired,
 };
 export default connect(mapStateToProp)(App);
