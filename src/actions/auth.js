fa254e97b6cf0311be82132137e77dd14d03a4c1
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2021-09-18 19:54:38 +0530

    Adding React Authentication for signUp

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,15 @@
 // import { PostsList } from '../components';
 import { APIUrls } from '../helpers/urls';
-import { LOGIN_FAILED, LOGIN_START, LOGIN_SUCCESS } from './actionTypes';
+import {
+  AUTHENTICATE_USER,
+  LOGIN_FAILED,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOG_OUT,
+  SIGNUP_FAILED,
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
+} from './actionTypes';
 import { getFormBody } from '../helpers/utils';
 
 export function startLogin() {
@@ -40,8 +49,70 @@ export function login(email, password) {
         if (data.success) {
           //dispatch action to save user
           dispatch(loginSuccess(data.data.user));
+          return;
         }
         dispatch(loginFailed(data.message));
       });
+  };
+}
+
+export default function authenticateUser(user) {
+  return {
+    type: AUTHENTICATE_USER,
+    user,
+  };
+}
+
+export function logoutUser() {
+  return {
+    type: LOG_OUT,
+  };
+}
+
+export function signup(firstName, lastName, email, password, confirmPassword) {
+  return (dispatch) => {
+    const url = APIUrls.signup();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-ww-form-urlencoded',
+      },
+      body: getFormBody({
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('signData', data);
+        if (data.success) {
+          localStorage.setItem('token', data.data.token);
+          dispatch(signupSuccessful(data.data.user));
+          return;
+        }
+        dispatch(signupFailed(data.message));
+      });
+  };
+}
+
+export function signupStart() {
+  return {
+    type: SIGNUP_START,
+  };
+}
+
+export function signupSuccessful(user) {
+  return {
+    type: SIGNUP_SUCCESS,
+    user,
+  };
+}
+export function signupFailed(error) {
+  return {
+    type: SIGNUP_FAILED,
+    error,
   };
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,40 +1,123 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signup, signupStart } from '../actions/auth';
 
-export default class SignUp extends Component {
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+  handleFirstNameChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      firstName: e.target.value,
+    });
+  };
+  handleLastNameChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
+  handleEmailChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  handleConfirmPasswordChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      confirmPassword: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const { firstName, lastName, email, password, confirmPassword } =
+      this.state;
+    if (firstName && lastName && email && password === confirmPassword) {
+      this.props.dispatch(signupStart());
+      this.props.dispatch(signup(firstName, lastName, email, password));
+    }
+  };
   render() {
+    const { inProgress, error } = this.props.auth;
     return (
       <form className="login-form">
         <span className="login-signup-header">SignUp</span>
-        <div className="field">
-          <input type="fname" placeholder="First Name" required />
-        </div>
-        <div className="field">
-          <input type="lname" placeholder="Last Name" required />
-        </div>
-        <div className="field">
-          <input type="date" placeholder="Date of Birth" required />
-        </div>
-        <div className="field">
-          <input type="email" placeholder="Email" required />
-        </div>
+        {error && <div className="alert error-dialog">{error}</div>}
         <div className="field">
           <input
-            type="number"
-            placeholder="Contact Number"
-            size="10"
+            type="fname"
+            placeholder="First Name"
+            onChange={this.handleFirstNameChange}
             required
           />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="lname"
+            placeholder="Last Name"
+            onChange={this.handleLastNameChange}
+            required
+          />
+        </div>
+
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={this.handleEmailChange}
+            required
+          />
+        </div>
+
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={this.handlePasswordChange}
+            required
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Confirm Password" required />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            onChange={this.handleConfirmPasswordChange}
+            required
+          />
         </div>
         <div className="field">
-          <button> SignUp</button>
+          {inProgress ? (
+            <button onClick={this.handleFormSubmit} disabled={inProgress}>
+              Signing Up
+            </button>
+          ) : (
+            <button onClick={this.handleFormSubmit}>SignUp</button>
+          )}
         </div>
       </form>
     );
   }
 }
+
+const mapStateToProp = ({ auth }) => ({
+  auth,
+});
+
+export default connect(mapStateToProp)(SignUp);
